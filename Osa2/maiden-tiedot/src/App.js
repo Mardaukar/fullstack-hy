ac2b5e083d1151107099bcb401dd18f666c94935
Markus Huhtamäki <makehuhtamaki@hotmail.com>
2019-11-27 11:32:05 +0200

    Säätehtävä lisätty

--- Osa2/maiden-tiedot/src/App.js
+++ Osa2/maiden-tiedot/src/App.js
@@ -4,6 +4,57 @@ import axios from 'axios'
 const App = () => {
   const [ countries, setCountries] = useState([]) 
   const [ newFilter, setNewFilter ] = useState('')
+  const [ weather, setWeather ] = useState(
+    {
+    "coord": {"lon": -122.08,"lat": 37.39},
+    "weather": [
+      {
+        "id": 800,
+        "main": "Clear",
+        "description": "clear sky",
+        "icon": "01d"
+      }
+    ],
+    "base": "stations",
+    "main": {
+      "temp": 296.71,
+      "pressure": 1013,
+      "humidity": 53,
+      "temp_min": 294.82,
+      "temp_max": 298.71
+    },
+    "visibility": 16093,
+    "wind": {
+      "speed": 1.5,
+      "deg": 350
+    },
+    "clouds": {
+      "all": 1
+    },
+    "dt": 1560350645,
+    "sys": {
+      "type": 1,
+      "id": 5122,
+      "message": 0.0139,
+      "country": "US",
+      "sunrise": 1560343627,
+      "sunset": 1560396563
+    },
+    "timezone": -25200,
+    "id": 420006353,
+    "name": "Mountain View",
+    "cod": 200
+  })
+
+  const [ currentCity, setCurrentCity ] = useState('helsinki')
+
+  const getter = () => {
+    axios
+        .get('https://api.openweathermap.org/data/2.5/weather?q=' + currentCity + '&appid=e47f22bdb6bedf9e9f825bc1c1e1e8d9')
+        .then(response => {
+          setWeather(response.data)
+        })
+  }
 
   useEffect(() => {
     axios
@@ -22,6 +73,11 @@ const App = () => {
     if (filteredcountries.length > 10) {
       return "Too many matches, specify another filter."
     } else if (filteredcountries.length === 1) {
+      setCurrentCity(filteredcountries[0].capital)
+      console.log(currentCity)
+      setTimeout(function(){
+        getter();
+      }, 10000);
       return <div><Country country={filteredcountries[0]} /></div>
     } else {
       return (filteredcountries.map(country => <FilteredCountry key={country.name} country={country}/>))
@@ -47,6 +103,10 @@ const App = () => {
           {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
         </ul>
         <img src={country.flag} height="60" width="60"></img>
+        <h2>weather in {country.capital}</h2>
+        <p>{weather.weather[0].description}</p>
+        <p>temperature: {Math.floor(weather.main.temp - 273)} Celsius</p>
+        <p>wind {weather.wind.speed} to direction {weather.wind.deg}</p>
       </>
     )
   }
